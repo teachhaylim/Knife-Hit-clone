5be0359138648c7b2d51bccbb045379724076bf1
Kimhak <kolkimhak99@yahoo.com>
2021-07-25 20:24:55 +0700

    added userData

---
+++ Assets/Scripts/UserData.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserData
+{
+    public int score = 0;
+    public string knife_sprite = "knife_0";
+
+    public void SaveData()
+    {
+
+    }
+
+    public void LoadData()
+    {
+
+    }
+
+    public UserData() { }
+    public UserData(int _score, string _knife_sprite)
+    {
+        this.score = _score;
+        this.knife_sprite = _knife_sprite;
+    }
+}
